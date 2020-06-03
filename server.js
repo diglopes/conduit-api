d58f374e52845d0dd60b63596a7d3bee14b3bd0f
Diego Lopes <diego-chez@hotmail.com>
2020-06-03 07:57:27 -0300

    feat: add logger to access

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const { createWriteStream } = require("fs");
+const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
+
+const accessLogStream = createWriteStream(
+  path.join(__dirname, "logs", "access.log"),
+  { flags: "a" }
+);
 
 const app = express();
 app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 module.exports = app;
