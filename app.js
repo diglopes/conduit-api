3f29411036b1066668c8d6606fa64c6b8eec4322
Diego Lopes <diego-chez@hotmail.com>
2020-06-03 09:01:40 -0300

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 "use strict";
 
+const mongoose = require("mongoose");
+const config = require("./config");
 const server = require("./server");
 
-const runningServer = server.listen(process.env.PORT || 3000, () => {
-  console.log(`>> Listening on port ${runningServer.address().port}`);
-});
+const init = async () => {
+  try {
+    await mongoose.connect(config.mongodbUri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    if (mongoose.connection.readyState === 1) {
+      console.log("[mongodb] Connection established successfully");
+    }
+    const runningServer = server.listen(process.env.PORT || 3000, () => {
+      console.log(`[server] Listening on port ${runningServer.address().port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
