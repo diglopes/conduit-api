71726446bd286a374eab9f2d0285d007e16cdec5
Diego Lopes <diego-chez@hotmail.com>
2020-06-03 08:14:07 -0300

    feat: handle errors on prod and dev modes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const { createWriteStream } = require("fs");
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const errorHandler = require("errorhandler");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const accessLogStream = createWriteStream(
   path.join(__dirname, "logs", "access.log"),
@@ -11,5 +14,33 @@ const accessLogStream = createWriteStream(
 const app = express();
 app.use(express.json());
 app.use(morgan("combined", { stream: accessLogStream }));
+
+// Handling error on development mode
+if (!isProduction) {
+  app.use(errorHandler());
+
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// Handling error on production mode
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
 
 module.exports = app;
