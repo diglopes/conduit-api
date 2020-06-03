368860aadeb492248d4c0dee598bf0e9dc2a9334
Diego Lopes <diego-chez@hotmail.com>
2020-06-03 18:17:00 -0300

    feat: create and validate user hash

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const crypto = require('crypto')
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -24,5 +25,15 @@ const UserSchema = new mongoose.Schema({
 }, {
   timestamps: true
 })
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex')
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+}
+
+UserSchema.methods.validatePassword = function (password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+  return this.hash === hash
+}
 
 mongoose.model('User', UserSchema)
