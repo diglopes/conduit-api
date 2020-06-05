30c4779b1922cf9480b610b829099d3e0db94b7a
Diego Lopes <diego-chez@hotmail.com>
2020-06-05 08:52:50 -0300

    refactor: add a describe as wrapper

--- src/routes/api/users/users.spec.js
+++ src/routes/api/users/users.spec.js
@@ -3,7 +3,7 @@ const database = require('../../../config/database')
 const UserModel = require('./user.model')
 const USERS_MOCK = require('../../../../mocks/USERS')
 
-describe('usersController::create', () => {
+describe('UsersController', () => {
   beforeAll(async () => {
     await database.connect()
   })
@@ -16,61 +16,51 @@ describe('usersController::create', () => {
     await database.disconnect()
   })
 
-  it('should create a user', async () => {
-    const httpRequest = USERS_MOCK.httpRequest.newUser
-    const user = await usersController.create(httpRequest)
-    expect(user.data.username).toBe(httpRequest.body.user.username)
-    expect(user.data.email).toBe(httpRequest.body.user.email)
-  })
+  describe('create', () => {
+    it('should create a user', async () => {
+      const httpRequest = USERS_MOCK.httpRequest.newUser
+      const user = await usersController.create(httpRequest)
+      expect(user.data.username).toBe(httpRequest.body.user.username)
+      expect(user.data.email).toBe(httpRequest.body.user.email)
+    })
 
-  it('should throw if username is already registered', async () => {
-    const httpRequest = USERS_MOCK.httpRequest.newUser
-    const httpRequestDiferentEmail = Object.assign({ body: { user: { email: 'difentEmail@email.com' } } }, httpRequest)
-    await usersController.create(httpRequest)
-    const user = await usersController.create(httpRequestDiferentEmail)
-    expect(user).toHaveProperty('error')
-    expect(user.error.message).toMatch(/duplicate/gi)
-  })
+    it('should throw if username is already registered', async () => {
+      const httpRequest = USERS_MOCK.httpRequest.newUser
+      const httpRequestDiferentEmail = Object.assign({ body: { user: { email: 'difentEmail@email.com' } } }, httpRequest)
+      await usersController.create(httpRequest)
+      const user = await usersController.create(httpRequestDiferentEmail)
+      expect(user).toHaveProperty('error')
+      expect(user.error.message).toMatch(/duplicate/gi)
+    })
 
-  it('should throw if email is already registered', async () => {
-    const httpRequest = USERS_MOCK.httpRequest.newUser
-    const httpRequestDiferentUsername =
+    it('should throw if email is already registered', async () => {
+      const httpRequest = USERS_MOCK.httpRequest.newUser
+      const httpRequestDiferentUsername =
       Object.assign({ body: { user: { username: 'diferentUsername' } } }, httpRequest)
-    await usersController.create(httpRequest)
-    const user = await usersController.create(httpRequestDiferentUsername)
-    expect(user).toHaveProperty('error')
-    expect(user.error.message).toMatch(/duplicate/gi)
-  })
-})
-
-describe('usersController::login', () => {
-  beforeAll(async () => {
-    await database.connect()
+      await usersController.create(httpRequest)
+      const user = await usersController.create(httpRequestDiferentUsername)
+      expect(user).toHaveProperty('error')
+      expect(user.error.message).toMatch(/duplicate/gi)
+    })
   })
 
-  afterEach(async () => {
-    await UserModel.deleteMany({})
-  })
+  describe('login', () => {
+    it('should return a user when valid credentials are provided', async () => {
+      const httpRequest = USERS_MOCK.httpRequest.newUser
+      await usersController.create(httpRequest)
+      const httpResponse = await usersController.login(httpRequest)
+      const props = ['token', 'email', 'username']
+      props.forEach(prop => expect(httpResponse.user).toHaveProperty(prop))
+      expect(httpResponse.user.email).toBe(httpRequest.body.user.email)
+    })
 
-  afterAll(async () => {
-    await database.disconnect()
-  })
-
-  it('should return a user when valid credentials are provided', async () => {
-    const httpRequest = USERS_MOCK.httpRequest.newUser
-    await usersController.create(httpRequest)
-    const httpResponse = await usersController.login(httpRequest)
-    const props = ['token', 'email', 'username']
-    props.forEach(prop => expect(httpResponse.user).toHaveProperty(prop))
-    expect(httpResponse.user.email).toBe(httpRequest.body.user.email)
-  })
-
-  it('should return a error with status 422 when invalid credentials are provided', async () => {
-    const httpRequest = USERS_MOCK.httpRequest.newUser
-    await usersController.create(httpRequest)
-    httpRequest.body.user.password = 'invalid_password'
-    const httpResponse = await usersController.login(httpRequest)
-    expect(httpResponse).toHaveProperty('error')
-    expect(httpResponse.error.status).toBe(422)
+    it('should return a error with status 422 when invalid credentials are provided', async () => {
+      const httpRequest = USERS_MOCK.httpRequest.newUser
+      await usersController.create(httpRequest)
+      httpRequest.body.user.password = 'invalid_password'
+      const httpResponse = await usersController.login(httpRequest)
+      expect(httpResponse).toHaveProperty('error')
+      expect(httpResponse.error.status).toBe(422)
+    })
   })
 })
