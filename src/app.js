dd38873916dd8b1359783a0ef151d6fc7da47a09
Diego Lopes <diego-chez@hotmail.com>
2020-06-05 09:01:48 -0300

    refactor: switch names between app and server

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
 'use strict'
 
-const server = require('./server')
-const database = require('./config/database')
+const { createWriteStream } = require('fs')
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const errorHandler = require('errorhandler')
+const {
+  cors,
+  notFound,
+  devErrorHandler,
+  prodErrorHandler
+} = require('./middlewares')
 
-const init = async () => {
-  try {
-    await database.connect()
-    const runningServer = server.listen(process.env.PORT || 3000, () => {
-      console.log(`[server] Listening on port ${runningServer.address().port}`)
-    })
-  } catch (error) {
-    console.log(error)
-  }
+require('./config/validationLocale')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const accessLogStream = createWriteStream(
+  path.join(__dirname, '..', 'logs', 'access.log'),
+  { flags: 'a' }
+)
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('combined', { stream: accessLogStream }))
+
+app.use(require('./routes'))
+
+// Catch 404 and forward to error handler
+app.use(notFound())
+
+// Handling error on development mode
+if (!isProduction) {
+  app.use(errorHandler())
+  app.use(devErrorHandler())
 }
 
-init()
+// Handling error on production mode
+app.use(prodErrorHandler())
+
+module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,43 +1,17 @@
 'use strict'
 
-const { createWriteStream } = require('fs')
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const errorHandler = require('errorhandler')
-const {
-  cors,
-  notFound,
-  devErrorHandler,
-  prodErrorHandler
-} = require('./middlewares')
+const app = require('./app')
+const database = require('./config/database')
 
-require('./config/validationLocale')
-
-const isProduction = process.env.NODE_ENV === 'production'
-
-const accessLogStream = createWriteStream(
-  path.join(__dirname, '..', 'logs', 'access.log'),
-  { flags: 'a' }
-)
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(morgan('combined', { stream: accessLogStream }))
-
-app.use(require('./routes'))
-
-// Catch 404 and forward to error handler
-app.use(notFound())
-
-// Handling error on development mode
-if (!isProduction) {
-  app.use(errorHandler())
-  app.use(devErrorHandler())
+const init = async () => {
+  try {
+    await database.connect()
+    const runningServer = app.listen(process.env.PORT || 3000, () => {
+      console.log(`[server] Listening on port ${runningServer.address().port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-// Handling error on production mode
-app.use(prodErrorHandler())
-
-module.exports = app
+init()
